13e2986b66973e1eb67ce9cabd4b1e73419dbda3
JiayiLiuCA <liujiayi402@gmail.com>
2020-07-17 00:31:21 -0400

    CSS grid auto-fill

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Item = ({item}) => {
     //console.log(item.artistName);
-    const artwork = item.artworkUrl60.slice(0,-11).concat("200x200bb.jpg")
+    const artwork = item.artworkUrl60.slice(0,-11).concat("170x170bb.jpg")
     return (
         <div>
             <img src={artwork} alt="Artwork" />
