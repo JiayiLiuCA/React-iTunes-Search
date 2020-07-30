68bf6234336866366d40b17986f958feb97f5dd4
JiayiLiuCA <liujiayi402@gmail.com>
2020-07-29 22:46:53 -0400

    Update Modal style

--- src/Components/ItemModal.js
+++ src/Components/ItemModal.js
@@ -3,10 +3,12 @@ import React from 'react'
 
 import '../Styles/ItemModal.css'
 
+import appleMusicBadge from '../img/apple-music-badge.svg'
+import iTunesMusicBadge from '../img/itunes-music-badge.svg'
+
 // fix appElement warning
 // https://github.com/reactjs/react-modal/issues/576
 ReactModal.setAppElement('body');
-
 
 const ItemModal = ({ isOpen, handleOverlayClick, modalItem }) => {
 
@@ -20,20 +22,32 @@ const ItemModal = ({ isOpen, handleOverlayClick, modalItem }) => {
                         backgroundColor: 'rgba(0,0,0,0.65)',
                     },
                     content: {
-                        top: '0',
+                        top: '-100px',
+                        left: '0',
+                        bottom: '0',
+                        right: '0',
                         height: '500px',
-                        width: '75vw',
-                        minWidth: '500px',
-                        maxWidth: '1000px',
+                        width: '80vw',
+                        minWidth: '800px',
+                        maxWidth: '1100px',
                         backgroundColor: 'rgba(225,225,225,.95)',
-                        padding: '50px',
+                        alignItems: 'center',
+                        paddingTop: '50px',
+                        paddingBottom: '50px',
+                        paddingLeft: '0px',
+                        paddingRight: '0px'
                     }
                 }}
             >
                 {/*check if modalItem is undefined first*/}
                 {modalItem &&
                     <div className="modal-container">
-                        <img src={modalItem.artworkUrl60.slice(0, -11).concat("400x400bb.jpg")} alt="artwork" />
+                        <div className="modal-item-img">
+                            <img
+                                src={modalItem.artworkUrl60.slice(0, -11).concat("400x400bb.jpg")}
+                                alt="artwork"
+                            />
+                        </div>
                         <div className="modal-item-data">
                             <div className="modal-item-metadata">
                                 <h1 style={{ fontFamily: 'Lato,sans-serif' }}>{modalItem.collectionName}</h1>
@@ -41,7 +55,7 @@ const ItemModal = ({ isOpen, handleOverlayClick, modalItem }) => {
                                 <br />
                                 {/*https://stackoverflow.com/a/650031/5228093*/}
                                 {modalItem.artistName.split(/ & |, /).map(artist => (
-                                    <p style={{fontFamily: 'Ubuntu,sans-serif', fontSize: '18px',}} key={artist}>
+                                    <p style={{ fontFamily: 'Ubuntu,sans-serif', fontSize: '18px', }} key={artist}>
                                         {artist}
                                     </p>
                                 ))}
@@ -54,7 +68,7 @@ const ItemModal = ({ isOpen, handleOverlayClick, modalItem }) => {
                                 {/*External links to apple*/}
                                 <a href={modalItem.collectionViewUrl}
                                     style={{
-                                        background: 'url(https://linkmaker.itunes.apple.com/en-us/badge-lrg.svg?kind=album)',
+                                        background: `url(${appleMusicBadge})`,
                                         width: '158px',
                                         height: '45px'
                                     }}
@@ -64,7 +78,7 @@ const ItemModal = ({ isOpen, handleOverlayClick, modalItem }) => {
                                 </a>
                                 <a href={`itms${modalItem.collectionViewUrl.slice(5)}&app=itunes`}
                                     style={{
-                                        background: 'url(https://linkmaker.itunes.apple.com/en-us/badge-lrg.svg)',
+                                        background: `url(${iTunesMusicBadge})`,
                                         backgroundRepeat: 'no-repeat',
                                         backgroundSize: 'cover',
                                         width: '158px',
