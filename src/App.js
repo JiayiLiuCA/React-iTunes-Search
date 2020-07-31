3c3056c49e924aed8ad960c7284f860259339170
JiayiLiuCA <liujiayi402@gmail.com>
2020-07-31 17:26:26 -0400

    Fix react-ga

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Header from './Components/Header';
 //Google Analytics
 import ReactGA from 'react-ga';
 ReactGA.initialize('UA-174170152-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 
 const App = () => {
